3325d518a11d218131ac569a80093bc2124a3025
Dales10 <matiasdario75@gmail.com>
2023-03-16 16:28:16 -0300

    Fixes 2.

--- pages/skills/softSkills.tsx
+++ pages/skills/softSkills.tsx
@@ -2,7 +2,7 @@ import style from '../../styles/title.module.scss';
 
 const SoftSkills = () => {
     return (
-        <div className="mt-24 lg:mt-32 mx-10 md:mx-16 xl:ml-28">
+        <div className='mt-24 lg:mt-32 mx-10 md:mx-16 xl:ml-28'>
             <span className='block w-[350px] sm:w-125 lg:w-[700px] h-[350px] sm:h-125 lg:h-[700px] absolute top-0 sm:-top-5 lg:-top-32 left-0 sm:-left-5 lg:-left-[155px] bg-[#0C009C] opacity-30 blur-[100px] sm:blur-[200px] -z-10' />
 
             <div className='w-full flex flex-col items-center md:items-start text-center md:text-left'>
