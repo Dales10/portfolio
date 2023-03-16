b98277f52f5fa8b62e18f844cf2fe6d3a4266247
Dales10 <matiasdario75@gmail.com>
2023-03-16 10:57:04 -0300

    Second part progress.

--- pages/skills/hardSkills.tsx
+++ pages/skills/hardSkills.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from '../../styles/hardSkills.module.scss';
 import style from '../../styles/title.module.scss';
 
-const knownTechnologies = [
+const currentSoftSkills = [
     'TypeScript',
     'React',
     'Next.js',
@@ -20,6 +20,19 @@ const knownTechnologies = [
     'Fastify',
 ];
 
+const futureSoftSkills = [
+    'C#',
+    'C++',
+    'Python',
+    'Kotlin',
+    'Lua',
+    'Nest.js',
+    'React Native',
+    'Mongodb',
+    'GraphQL',
+    '.Net',
+];
+
 const hardSkills = () => {
     return (
         <div className="mt-24 lg:mt-32 mx-10 md:mx-16 xl:ml-28">
@@ -35,22 +48,39 @@ const hardSkills = () => {
                 </p>
             </div>
 
-            <div className='w-full lg:w-[calc(100%_-_100px)] flex flex-col items-center border-t-4 border-zinc-900 mt-40 xs:mt-52 md:mt-72 mb-28'>
-                <h2 className='text-4xl md:text-5xl text-center font-extrabold my-44'>
+            <div className='w-full lg:w-[calc(100%_-_100px)] border-t-4 border-zinc-900 pt-44 xs:mt-52 md:mt-72 mb-36'>
+                <h2 className='text-4xl md:text-5xl text-center font-extrabold'>
                     Minhas Hard Skills
                 </h2>
 
-                <div className='flex flex-wrap justify-center gap-6'>
+                <div className='max-w-[800px] flex flex-col gap-2 font-extrabold'>
+                    <h2 className='text-5xl '>
+                        Firebase
+                    </h2>
+                    <p className='text-2xl'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum cupiditate perspiciatis error fuga ipsum accusamus incidunt voluptates architecto, ad corrupti. Sequi corporis est maiores nisi vero commodi, qui vitae?
+                    </p>
+                </div>
+
+                <div className='flex flex-wrap justify-between gap-6 mt-44 pb-28 border-b-4 border-zinc-900'>
                     {
-                        knownTechnologies.map(technologie => {
+                        currentSoftSkills.map(technologie => {
                             return (
-                                <div className={`${styles.card} flex justify-center items-center w-36 h-36 bg-background border border-[#00DBDE] rounded-md transition duration-300`}>
+                                <div
+                                    key={technologie}
+                                    className={`${styles.card} flex justify-center items-center w-36 h-36 bg-background border border-[#00DBDE] rounded-[5px] mx-auto transition duration-300`}
+                                >
                                     <div className='flex flex-col justify-between items-center gap-2 py-1'>
                                         <Image
-                                            src={`/icons/${technologie.toLocaleLowerCase()}.png`}
+                                            src={`/icons/${technologie
+                                                .toLocaleLowerCase()
+                                                .replace(' ', '_')
+                                                .replace('#', '_sharp')
+                                                .replace('.net', 'dot_net')
+                                                }.png`}
                                             width={90}
                                             height={90}
-                                            alt='Icon of React'
+                                            alt={`${technologie} icon.`}
                                         />
                                         <h2 className='text-xl font-semibold'>
                                             {technologie}
@@ -62,7 +92,39 @@ const hardSkills = () => {
                     }
                 </div>
 
+                <h2 className='max-w-[1100px] text-4xl font-bold mt-36'>
+                    Mas também há as tecnologias que tenho interesse em aprender futuramente, ou quando tiver uma oportunidade, que são as seguintes:
+                </h2>
 
+                <div className='flex flex-wrap justify-between gap-6 mt-20'>
+                    {
+                        futureSoftSkills.map(technologie => {
+                            return (
+                                <div
+                                    key={technologie}
+                                    className={`${styles.card} flex justify-center items-center w-36 h-36 bg-background border border-[#00DBDE] rounded-[5px] mx-auto transition duration-300`}
+                                >
+                                    <div className='flex flex-col justify-between items-center gap-2 py-1'>
+                                        <Image
+                                            src={`/icons/${technologie
+                                                .toLocaleLowerCase()
+                                                .replace(' ', '_')
+                                                .replace('#', '_sharp')
+                                                .replace('.net', 'dot_net')
+                                                }.png`}
+                                            width={90}
+                                            height={90}
+                                            alt={`${technologie} icon.`}
+                                        />
+                                        <h2 className='text-xl font-semibold'>
+                                            {technologie}
+                                        </h2>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
