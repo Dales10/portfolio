b1cd87b65195dd28b750d6fa71d1ce6fdffd6833
Dales10 <matiasdario75@gmail.com>
2023-03-18 10:20:02 -0300

    Add visual feedback when composing the email.

--- pages/contato.tsx
+++ pages/contato.tsx
@@ -5,10 +5,11 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import emailjs from '@emailjs/browser';
+import { useState } from 'react';
 
 const schema = yup.object({
     name: yup.string().required('O nome é obrigatório.').min(5, 'O mínimo de caracteres é 5.').max(50, 'O máximo de caracteres é 50.'),
-    email: yup.string().required('O email é obrigatório.').email('O email deve conter um @, assim como caracteres antes e depois do mesmo. Logo após o @ deve existir um ponto seguido de caracteres.'),
+    email: yup.string().required('O email é obrigatório.').email('O email deve conter um @, assim como caracteres antes e depois do mesmo.'),
     subject: yup.string().required('O assunto/título do email é obrigatório.').min(5, 'O mínimo de caracteres é 5.').max(50, 'O máximo de caracteres é 50.'),
     message: yup.string().required('A mensagem/conteúdo do email é obrigatória.').min(5, 'O mínimo de caracteres é 5.').max(1024, 'O máximo de caracteres é 1024.'),
 }).required('Todos os campos são obrigatórios.');
@@ -16,9 +17,21 @@ const schema = yup.object({
 type DataProps = yup.InferType<typeof schema>;
 
 const Contato = () => {
-    const { register, handleSubmit, formState: { errors }, reset } = useForm<DataProps>({
+    const { register, handleSubmit, formState: { errors }, reset, watch } = useForm<DataProps>({
         resolver: yupResolver(schema),
     });
+
+    const [styleInputName, setStyleInputName] = useState({ borderColor: '#757575' })
+    let nameLength = watch('name')?.length || 0;
+
+    const [styleInputEmail, setStyleInputEmail] = useState({ borderColor: '#757575' })
+    let emailLength = watch('email')?.length || 0;
+
+    const [styleInputSubject, setStyleInputSubject] = useState({ borderColor: '#757575' })
+    let subjectLength = watch('subject')?.length || 0;
+
+    const [styleInputMessage, setStyleInputMessage] = useState({ borderColor: '#757575' })
+    let messageLength = watch('message')?.length || 0;
 
     const sendEmail = (data: DataProps) => {
         const template_params = {
@@ -33,121 +46,133 @@ const Contato = () => {
             process.env.NEXT_PUBLIC_TEMPLATE_ID!,
             template_params,
             process.env.NEXT_PUBLIC_PUBLIC_KEY!,
-            )
+        )
             .then(() => {
                 alert('Email enviado com sucesso!');
                 reset();
             })
-    .catch(err => {
-        console.log(err);
-    });
+            .catch(err => {
+                console.log(err);
+            });
     };
 
-return (
-    <div className='mt-24 lg:mt-32 mx-10 md:mx-16 xl:ml-28'>
-        <span className='block w-[350px] sm:w-125 lg:w-[700px] h-[350px] sm:h-125 lg:h-[700px] absolute top-0 sm:-top-5 lg:-top-32 left-0 sm:-left-5 lg:-left-[155px] bg-[#0C009C] opacity-30 blur-[100px] sm:blur-[200px] -z-10' />
+    return (
+        <div className='mt-24 lg:mt-32 mx-10 md:mx-16 xl:ml-28'>
+            <span className='block w-[350px] sm:w-125 lg:w-[700px] h-[350px] sm:h-125 lg:h-[700px] absolute top-0 sm:-top-5 lg:-top-32 left-0 sm:-left-5 lg:-left-[155px] bg-[#0C009C] opacity-30 blur-[100px] sm:blur-[200px] -z-10' />
 
-        <div className='w-full flex flex-col items-center md:items-start text-center md:text-left'>
-            <h1 className={`${style.title} text-5xl sm:text-6xl md:text-7xl lg:text-[80px] font-black leading-tight`}>
-                Contate-me
-            </h1>
+            <div className='w-full flex flex-col items-center md:items-start text-center md:text-left'>
+                <h1 className={`${style.title} text-5xl sm:text-6xl md:text-7xl lg:text-[80px] font-black leading-tight`}>
+                    Contate-me
+                </h1>
 
-            <p className='max-w-[500px] md:max-w-[700px] lg:max-w-[800px] xl:max-w-[900px] mt-12 lg:mt-20 text-xl sm:text-2xl md:text-3xl lg:text-4xl text-justify lg:text-start font-bold leading-tight'>
-                Envie-me um e-mail com a proposta que tiver para podermos negociar.
-            </p>
+                <p className='max-w-[500px] md:max-w-[700px] lg:max-w-[800px] xl:max-w-[900px] mt-12 lg:mt-20 text-xl sm:text-2xl md:text-3xl lg:text-4xl text-justify lg:text-start font-bold leading-tight'>
+                    Envie-me um e-mail com a proposta que tiver para podermos negociar.
+                </p>
+            </div>
+
+            <form
+                onSubmit={handleSubmit(sendEmail)}
+                className='max-w-[700px] flex flex-col gap-3 mb-20 sm:mb-24 mt-20 xs:mt-28'
+            >
+                <div className='flex flex-col md:flex-row gap-3'>
+                    <div className='w-full'>
+                        <div className={styles.input}>
+                            <input
+                                id='name'
+                                type='text'
+                                placeholder='Dário Matias'
+                                {...register('name')}
+                                style={errors.name?.message ? { borderColor: '#FF0000' } : styleInputName}
+                                onFocus={() => setStyleInputName({ borderColor: '#00DBDE' })}
+                                onBlur={() => { if (nameLength === 0) setStyleInputName({ borderColor: '#757575' }) }}
+                                className='w-full h-14 bg-[#11111B] border pt-2 pl-4 placeholder:font-medium placeholder:text-[#757575] outline-none'
+                            />
+                            <label htmlFor="name">
+                                Nome:
+                            </label>
+                        </div>
+
+                        <span className='text-sm text-red-600 font-bold'>
+                            {errors.name?.message}
+                        </span>
+                    </div>
+
+                    <div className='w-full'>
+                        <div className={styles.input}>
+                            <input
+                                id='email'
+                                type='text'
+                                placeholder='seuemail@gmail.com'
+                                {...register('email')}
+                                style={errors.email?.message ? { borderColor: '#FF0000' } : styleInputEmail}
+                                onFocus={() => setStyleInputEmail({ borderColor: '#00DBDE' })}
+                                onBlur={() => { if (emailLength === 0) setStyleInputEmail({ borderColor: '#757575' }) }}
+                                className='w-full h-14 bg-[#11111B] border pt-2 pl-4 placeholder:font-medium placeholder:text-[#757575] outline-none'
+                            />
+                            <label htmlFor="email">
+                                Email:
+                            </label>
+                        </div>
+
+                        <span className='text-sm text-red-600 font-bold'>
+                            {errors.email?.message}
+                        </span>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.input}>
+                        <input
+                            id='subject'
+                            type='text'
+                            placeholder='Aprender mais'
+                            {...register('subject')}
+                            style={errors.subject?.message ? { borderColor: '#FF0000' } : styleInputSubject}
+                            onFocus={() => setStyleInputSubject({ borderColor: '#00DBDE' })}
+                            onBlur={() => { if (subjectLength === 0) setStyleInputSubject({ borderColor: '#757575' }) }}
+                            className='w-full h-14 bg-[#11111B] border pt-2 pl-4 placeholder:font-medium placeholder:text-[#757575] outline-none'
+                        />
+                        <label htmlFor="subject">
+                            Assunto:
+                        </label>
+                    </div>
+
+                    <span className='text-sm text-red-600 font-bold'>
+                        {errors.subject?.message}
+                    </span>
+                </div>
+
+                <div>
+                    <div className={styles.input}>
+                        <textarea
+                            id='message'
+                            placeholder='Vamos estudar programação.'
+                            {...register('message')}
+                            style={errors.message?.message ? { borderColor: '#FF0000' } : styleInputMessage}
+                            onFocus={() => setStyleInputMessage({ borderColor: '#00DBDE' })}
+                            onBlur={() => { if (messageLength === 0) setStyleInputMessage({ borderColor: '#757575' }) }}
+                            className='h-44 w-full bg-[#11111B] border pt-5 pl-4 placeholder:font-medium placeholder:text-[#757575] resize-none outline-none'
+                        />
+                        <label htmlFor="message">
+                            Mensagem:
+                        </label>
+                    </div>
+
+                    <span className='text-sm text-red-600 font-bold'>
+                        {errors.message?.message}
+                    </span>
+                </div>
+
+                <div className='w-full flex justify-end'>
+                    <button className={`${styleButton.button} w-56 sm:w-[250px] h-20 sm:h-20 mt-8 text-2xl sm:text-3xl`}>
+                        Enviar
+                    </button>
+                </div>
+            </form>
+
+            <span className='block w-[300px] sm:w-100 lg:w-[400px] h-125 sm:h-[200px] lg:h-[450px] absolute top-[600px] sm:top-[790px] left-1/4 md:left-2/4 bg-[#0C009C] opacity-30 blur-[100px] sm:blur-[150px] -z-10' />
         </div>
-
-        <form
-            onSubmit={handleSubmit(sendEmail)}
-            className='max-w-[700px] flex flex-col gap-3 mb-20 sm:mb-24 mt-20 xs:mt-28'
-        >
-            <div className='flex flex-col md:flex-row gap-3'>
-                <div className='w-full'>
-                    <div className={styles.input}>
-                        <input
-                            id='name'
-                            type='text'
-                            placeholder='Dário Matias'
-                            {...register('name')}
-                            className='w-full h-14 bg-[#11111B] border border-[#00DBDE] pt-2 pl-4 placeholder:font-medium placeholder:text-[#757575] outline-none'
-                        />
-                        <label htmlFor="name">
-                            Nome:
-                        </label>
-                    </div>
-
-                    <span className='text-sm text-red-600 font-bold'>
-                        {errors.name?.message}
-                    </span>
-                </div>
-
-                <div className='w-full'>
-                    <div className={styles.input}>
-                        <input
-                            id='email'
-                            type='text'
-                            placeholder='seuemail@gmail.com'
-                            {...register('email')}
-                            className='w-full h-14 bg-[#11111B] border border-[#00DBDE] pt-2 pl-4 placeholder:font-medium placeholder:text-[#757575] outline-none'
-                        />
-                        <label htmlFor="email">
-                            Email:
-                        </label>
-                    </div>
-
-                    <span className='text-sm text-red-600 font-bold'>
-                        {errors.email?.message}
-                    </span>
-                </div>
-            </div>
-
-            <div>
-                <div className={styles.input}>
-                    <input
-                        id='subject'
-                        type='text'
-                        {...register('subject')}
-                        placeholder='Aprender mais'
-                        className='h-14 bg-[#11111B] border border-[#00DBDE] pt-2 pl-4 outline-none'
-                    />
-                    <label htmlFor="subject">
-                        Assunto:
-                    </label>
-                </div>
-
-                <span className='text-sm text-red-600 font-bold'>
-                    {errors.subject?.message}
-                </span>
-            </div>
-
-            <div>
-                <div className={styles.input}>
-                    <textarea
-                        id='message'
-                        placeholder='Vamos estudar programação.'
-                        {...register('message')}
-                        className='h-44 w-full bg-[#11111B] border border-[#00DBDE] pt-5 pl-4 placeholder:font-medium placeholder:text-[#757575] resize-none outline-none'
-                    />
-                    <label htmlFor="message">
-                        Mensagem:
-                    </label>
-                </div>
-
-                <span className='text-sm text-red-600 font-bold'>
-                    {errors.message?.message}
-                </span>
-            </div>
-
-            <div className='w-full flex justify-end'>
-                <button className={`${styleButton.button} w-56 sm:w-[250px] h-20 sm:h-20 mt-8 text-2xl sm:text-3xl`}>
-                    Enviar
-                </button>
-            </div>
-        </form>
-
-        <span className='block w-[300px] sm:w-100 lg:w-[400px] h-125 sm:h-[200px] lg:h-[450px] absolute top-[600px] sm:top-[790px] left-1/4 md:left-2/4 bg-[#0C009C] opacity-30 blur-[100px] sm:blur-[150px] -z-10' />
-    </div>
-);
+    );
 };
 
 export default Contato;
