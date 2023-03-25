f691e7380b29996d765373167c284f1c911c8eaf
Dales10 <matiasdario75@gmail.com>
2023-03-25 18:06:32 -0300

    First solution for missing input animation issue.

--- components/Context/index.tsx
+++ components/Context/index.tsx
@@ -1,5 +1,3 @@
-import AOS from 'aos';
-
 import { useState, useEffect, useContext, createContext, ReactNode } from "react";
 import { db, collection, getDocs } from '../../services/firebase';
 
@@ -51,9 +49,6 @@ export const DataProvider = ({ children }: DataProviderProps) => {
     };
 
     useEffect(() => {
-        AOS.init({
-            duration: 1200,
-        });
         readDataFromDatabase();
     }, []);
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.scss';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import SocialMedia from './../components/SocialMedia';
 import { DataProvider } from './../components/Context/index';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+		AOS.init({
+			delay: 0,
+			duration: 1200,
+		});
+   }, []);
+
   return (
     <>
       <Header />
